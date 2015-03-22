307b7274ab1ff4ffbef093dd7ede89d79440596b
jenshadlich <jenshadlich@googlemail.com>
2015-03-22 23:50:49 +0100

    add basic charsets

---
+++ crawler/src/main/java/de/jeha/kame/crawler/charsets/Charsets.java
@@ -0,0 +1,11 @@
+package de.jeha.kame.crawler.charsets;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class Charsets {
+    public static final Charset UTF_8 = Charset.forName("UTF-8");
+    public static final Charset ASCII = Charset.forName("US-ASCII");
+}
