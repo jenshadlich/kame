3aadb0123fde39d271d44201339796f90e225dfd
jenshadlich <jenshadlich@googlemail.com>
2015-03-22 22:07:28 +0100

    some initial code

---
+++ crawler/src/main/java/de/jeha/kame/crawler/Crawler.java
@@ -0,0 +1,61 @@
+package de.jeha.kame.crawler;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class Crawler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Crawler.class);
+
+    // Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
+    // TODO: setup external resource + add to user agent
+    private static final String USER_AGENT = "Mozilla/5.0 (compatible; Kamebot/0.1)";
+
+    private final String url;
+
+    public Crawler(String url) {
+        this.url = url;
+    }
+
+    public CrawlResult execute() throws IOException {
+        LOG.info("Crawling: '{}'", url);
+
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setHeader("User-Agent", USER_AGENT);
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        HttpResponse response = httpClient.execute(httpGet);
+
+        try {
+            StatusLine statusLine = response.getStatusLine();
+            LOG.debug(statusLine.toString());
+
+            HttpEntity entity = response.getEntity();
+            final String content = IOUtils.toString(entity.getContent());
+            EntityUtils.consume(entity);
+            final String contentType = entity.getContentType().getValue();
+
+            stopWatch.stop();
+
+            return new CrawlResult(url, content, contentType, statusLine.getStatusCode(), stopWatch.getTime());
+        } finally {
+            httpGet.releaseConnection();
+        }
+    }
+}
---
+++ crawler/src/main/java/de/jeha/kame/crawler/Main.java
@@ -0,0 +1,78 @@
+package de.jeha.kame.crawler;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    private static final int MAX = 1;
+    private static final boolean SAME_DOMAIN = true;
+    private static final String URL = "www.spreadshirt.de";
+
+    public static void main(String... args) throws IOException {
+        Map<String, Boolean> links = new HashMap<String, Boolean>();
+        links.put("http://" + URL, Boolean.FALSE);
+
+        int cnt = 0;
+        boolean done = false;
+        while (!done) {
+            Set<String> newLinks = new HashSet<String>();
+            for (String link : links.keySet()) {
+                boolean crawl = true;
+                if (links.containsKey(link)) {
+                    boolean alreadyCrawled = links.get(link);
+                    if (alreadyCrawled) {
+                        crawl = false;
+                    }
+                }
+                if (crawl) {
+                    cnt++;
+                    if (cnt > MAX) {
+                        done = true;
+                        break;
+                    }
+                    CrawlResult result = new Crawler(link).execute();
+                    LOG.debug("Status code: {}", result.getStatusCode());
+
+                    links.put(link, Boolean.TRUE);
+
+                    for (String rawNewLink : result.parse()) {
+                        String newLink = StringEscapeUtils.escapeHtml4(StringUtils.trim(rawNewLink));
+
+                        if (SAME_DOMAIN && !newLink.contains(URL)) {
+                            LOG.debug("Skip: {}", newLink);
+                            continue;
+                        }
+                        if (!newLinks.contains(newLink)) {
+                            try { // TODO: check validity
+                                URI uri = URI.create(newLink);
+                                LOG.debug(uri.toString());
+                                newLinks.add(newLink);
+                            } catch (IllegalArgumentException e) {
+                                LOG.warn("skip invalid url: '{}'", newLink);
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (String newLink : newLinks) {
+                links.put(newLink, Boolean.FALSE);
+            }
+        }
+        LOG.info("Number of links: {}", links.size());
+    }
+}
