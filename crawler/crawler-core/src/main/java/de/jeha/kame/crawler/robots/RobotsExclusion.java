dc38b5eba7f842143894652fa638932f20ee34b7
jenshadlich <jenshadlich@googlemail.com>
2015-05-19 18:17:15 +0200

    add robots.txt parser

---
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/robots/RobotsExclusion.java
@@ -0,0 +1,74 @@
+package de.jeha.kame.crawler.robots;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class RobotsExclusion {
+
+    private Map<String, List<String>> disallowMap = new HashMap<>();
+
+    public RobotsExclusion(Map<String, List<String>> disallowMap) {
+        this.disallowMap = disallowMap;
+        if (disallowMap.isEmpty()) {
+            this.disallowMap.put("*", Arrays.asList(""));
+        }
+    }
+
+    /**
+     * Check if a path is allow for a user agent.
+     *
+     * @param userAgent user agent
+     * @param path      path
+     * @return true if allowed
+     */
+    public boolean allowed(String userAgent, String path) {
+        return !disallowed(userAgent, path); // naive implementation
+    }
+
+    /**
+     * Check if a path is NOT allow for a user agent.
+     *
+     * @param userAgent user agent
+     * @param path      path
+     * @return true if NOT allowed
+     */
+    public boolean disallowed(String userAgent, String path) {
+        boolean disallowed;
+
+        if (!UserAgents.ANY.equals(userAgent)) {
+            disallowed = isDisallowed(userAgent, path);
+            if (!disallowed) {
+                disallowed = isDisallowed(UserAgents.ANY, path);
+            }
+        } else {
+            disallowed = isDisallowed(userAgent, path);
+        }
+        return disallowed;
+    }
+
+    private boolean isDisallowed(String userAgent, String path) {
+        // test specified userAgent
+        if (disallowMap.containsKey(userAgent.toLowerCase())) {
+            for (String disallowedPath : disallowMap.get(userAgent.toLowerCase())) {
+                // exact match
+                if (StringUtils.equalsIgnoreCase(path, disallowedPath)) {
+                    return true;
+                }
+                // disallow subdirectories
+                if (disallowedPath.endsWith("/") && StringUtils.startsWithIgnoreCase(path, disallowedPath)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
---
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/robots/RobotsExclusionParser.java
@@ -0,0 +1,82 @@
+package de.jeha.kame.crawler.robots;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Parser for robots.txt files. Currently limited to the original standard which means it does only support User-agent
+ * and Disallow instructions (no Allow, Crawl-Delay or Sitemap).
+ *
+ * See http://www.robotstxt.org/
+ *
+ * @author jenshadlich@googlemail.com
+ */
+public class RobotsExclusionParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RobotsExclusionParser.class);
+
+    /**
+     * Parse a given robots.txt file given as InputStream.
+     *
+     * @param robotsFileInputStream robots.txt
+     * @return robots.txt object model to query
+     * @throws IOException
+     */
+    public RobotsExclusion parse(InputStream robotsFileInputStream) throws IOException {
+        List<String> lines = IOUtils.readLines(robotsFileInputStream);
+
+        Map<String, List<String>> disallowMap = new HashMap<>();
+        int lineCounter = 0;
+        String currentUserAgent = null;
+
+        for (String line : lines) {
+            ++lineCounter;
+            final String trimmedLine = line.trim();
+            if (trimmedLine.startsWith(Constants.COMMENT_DELIM)) {
+                continue;
+            }
+            final String trimmedLineWithoutComments = StringUtils.substringBefore(trimmedLine, Constants.COMMENT_DELIM);
+
+            if (trimmedLineWithoutComments.isEmpty()) {
+                continue;
+            }
+
+            String parts[] = trimmedLineWithoutComments.split(Constants.FIELD_DELIM);
+            if (parts.length < 1 || parts.length > 2) {
+                LOG.warn("skip line {}, illegal format: '{}'", lineCounter, line);
+                continue;
+            }
+            final String field = parts[0].trim().toLowerCase();
+            final String value = (parts.length > 1) ? parts[1].trim() : "";
+
+            switch (field) {
+                case Constants.USER_AGENT:
+                    currentUserAgent = value.toLowerCase();
+                    break;
+                case Constants.DISALLOW:
+                    if (currentUserAgent == null) {
+                        LOG.warn("skip line {}, " + Constants.DISALLOW + " without prior " + Constants.USER_AGENT, lineCounter);
+                    } else {
+                        if (!disallowMap.containsKey(currentUserAgent)) {
+                            disallowMap.put(currentUserAgent, new ArrayList<>());
+                        }
+                        disallowMap.get(currentUserAgent).add(value);
+                    }
+                    break;
+                default:
+                    LOG.info("skip line {}, unsupported instruction: '{}'", lineCounter, line);
+                    break;
+            }
+        }
+        return new RobotsExclusion(disallowMap);
+    }
+}
