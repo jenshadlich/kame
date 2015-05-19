eebae79e0ed38e0ea6e8f4e9ace9fbd44bcebc93
jenshadlich <jenshadlich@googlemail.com>
2015-05-19 18:25:06 +0200

    add constants for nonstandard extensions

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/robots/Constants.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/robots/Constants.java
@@ -7,13 +7,13 @@ public final class Constants {
 
     public static final String ROBOTS_FILE = "/robots.txt";
 
-    public static final String USER_AGENT = "user-agent"; // field "User-agent", lower case
-    public static final String DISALLOW = "disallow"; // field "Disallow", lower case
+    public static final String USER_AGENT = "user-agent"; // field "User-agent"
+    public static final String DISALLOW = "disallow"; // field "Disallow"
 
     // currently unsupported nonstandard extensions:
-    // Crawl-delay
-    // Allow
-    // Sitemap
+    public static final String ALLOW = "allow"; // field "Allow"
+    public static final String CRAWL_DELAY = "crawl-delay"; // field "Crawl-delay"
+    public static final String SITEMAP = "sitemap"; // field "Sitemap"
 
     public static final String COMMENT_DELIM = "#";
     public static final String FIELD_DELIM = ":";
