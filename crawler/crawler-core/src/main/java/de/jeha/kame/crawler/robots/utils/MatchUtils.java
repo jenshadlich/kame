9b82a6dfd5195f14c85732303c79fcd1f89fa242
jenshadlich <jenshadlich@googlemail.com>
2015-05-21 08:35:11 +0200

    refactor

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/robots/utils/MatchUtils.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/robots/utils/MatchUtils.java
@@ -7,25 +7,29 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class MatchUtils {
 
+    private static final char WILDCARD_C = '*';
+    private static final String WILDCARD = "" + WILDCARD_C;
+    private static final String DOLLAR_SIGN = "$";
+
     /**
      * @param text    text to match against pattern
      * @param pattern pattern with wildcard
      * @return true if text match the pattern
      */
     public static boolean wildcardMatch(final String text, final String pattern) {
-        String[] parts = StringUtils.split(pattern, '*');
+        String[] parts = StringUtils.split(pattern, WILDCARD_C);
         String remainder = text;
 
         // no match possible if the 'text' does not start with the first part
-        if (parts.length > 0 && !pattern.startsWith("*") && !remainder.startsWith(parts[0])) {
+        if (parts.length > 0 && !pattern.startsWith(WILDCARD) && !remainder.startsWith(parts[0])) {
             return false;
         }
 
         for (String part : parts) {
-            boolean hasEndChar = part.endsWith("$");
+            boolean hasEndChar = part.endsWith(DOLLAR_SIGN);
 
             if (hasEndChar) {
-                part = StringUtils.substringBefore(part, "$");
+                part = StringUtils.substringBefore(part, DOLLAR_SIGN);
             }
 
             int idx = remainder.indexOf(part);
@@ -42,7 +46,7 @@ public class MatchUtils {
         }
 
         // if the pattern does not end with a wildcard the remainder must be empty
-        return remainder.isEmpty() || pattern.endsWith("*");
+        return remainder.isEmpty() || pattern.endsWith(WILDCARD);
     }
 
 }
