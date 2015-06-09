821da8afe76c8d9f08284f0ece46679a56298078
jenshadlich <jenshadlich@googlemail.com>
2015-06-09 08:19:50 +0200

    add more header constants

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/types/Headers.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/types/Headers.java
@@ -9,11 +9,19 @@ import java.util.Map;
 public class Headers {
 
     public static final String CACHE_CONTROL = "Cache-Control";
+    public static final String CONTENT_ENCODING = "Content-Encoding";
+    public static final String CONTENT_LANGUAGE = "Content-Language";
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String DATE = "Date";
+    public static final String ETAG = "ETag";
+    public static final String EXPIRES = "Expires";
+    public static final String LAST_MODIFIED = "Last-Modified";
+    public static final String RETRY_AFTER = "Retry-After";
     public static final String PRAGMA = "Pragma";
     public static final String SERVER = "Server";
     public static final String X_ROBOTS_TAG = "X-Robots-Tag";
+    public static final String WWW_AUTHENTICATE = "WWW-Authenticate";
 
     private final Map<String, String> headers = new HashMap<>();
 
