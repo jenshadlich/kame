45e2067a10276a832e3bcd97df032a9a5b161e83
jenshadlich <jenshadlich@googlemail.com>
2015-06-23 15:17:48 +0200

    refactor

--- crawler/crawler-service/src/main/java/de/jeha/kame/crawler/service/api/CrawlRequest.java
+++ crawler/crawler-service/src/main/java/de/jeha/kame/crawler/service/api/CrawlRequest.java
@@ -1,17 +1,17 @@
 package de.jeha.kame.crawler.service.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * @author jenshadlich@googlemail.com
  */
 public class CrawlRequest {
 
+    @JsonProperty
     private String url;
 
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
