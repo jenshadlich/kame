75d0bcac992c83247a3c789d35d797056f001fc6
jenshadlich <jenshadlich@googlemail.com>
2015-06-23 21:56:26 +0200

    add domain to page

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Page.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Page.java
@@ -4,4 +4,11 @@ package de.jeha.kame.crawler.core.model;
  * @author jenshadlich@googlemail.com
  */
 public class Page {
+
+    private final Domain domain;
+
+    public Page(Domain domain) {
+        this.domain = domain;
+    }
+
 }
