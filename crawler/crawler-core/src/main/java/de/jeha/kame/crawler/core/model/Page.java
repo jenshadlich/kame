34601d319e2b6117d31c4825a066223762f4f223
jenshadlich <jenshadlich@googlemail.com>
2015-06-25 21:25:38 +0200

    start implementation of Page builder

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Page.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Page.java
@@ -1,5 +1,15 @@
 package de.jeha.kame.crawler.core.model;
 
+import de.jeha.kame.crawler.core.LinkExtractor;
+import de.jeha.kame.crawler.core.robots.RobotsMetaContentExtractor;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.List;
+
 /**
  * @author jenshadlich@googlemail.com
  */
@@ -8,11 +18,13 @@ public class Page {
     private final Domain domain;
     private final Hash hash;
     private final String url;
+    private final List<String> links;
 
-    public Page(Domain domain, String url) {
+    public Page(Domain domain, String url, List<String> links) {
         this.domain = domain;
         this.hash = new Hash(url);
         this.url = url;
+        this.links = links;
     }
 
     public Domain getDomain() {
@@ -27,4 +39,50 @@ public class Page {
         return url;
     }
 
+    public List<String> getLinks() {
+        return links;
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public static class Builder {
+
+        private static final LinkExtractor LINK_EXTRACTOR = new LinkExtractor();
+        private static final RobotsMetaContentExtractor ROBOTS_META_CONTENT_EXTRACTOR = new RobotsMetaContentExtractor();
+
+        private String url;
+        private String content;
+
+        public Builder withUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder withContent(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Page build() {
+            Document document = Jsoup.parse(content);
+            List<String> links = LINK_EXTRACTOR.get(document);
+
+            String domain = extractDomainName(url);
+            return new Page(new Domain(domain), url, links);
+        }
+
+        public static Builder New() {
+            return new Builder();
+        }
+
+        private String extractDomainName(String url) {
+            return StringUtils.substringBefore(StringUtils.substringAfter(url, "://"), "/").replaceFirst("^www.*?\\.", "");
+        }
+
+    }
 }
