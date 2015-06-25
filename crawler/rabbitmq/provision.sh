a8c0d70b97861fa0b078cddef45ea5a173137351
jenshadlich <jenshadlich@googlemail.com>
2015-06-25 17:46:22 +0200

    update

--- crawler/rabbitmq/provision.sh
+++ crawler/rabbitmq/provision.sh
@@ -2,10 +2,12 @@
 export DEBIAN_FRONTEND=noninteractive
 
 wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-sudo apt-key add rabbitmq-signing-key-public.asc
+apt-key add rabbitmq-signing-key-public.asc
 
 apt-get update > /dev/null
-apt-get -y install rabbitmq-server
+apt-get -q -y install rabbitmq-server
 
+# enable RabbitMQ plugins
+service rabbitmq-server stop
 rabbitmq-plugins enable rabbitmq_management
-service rabbitmq-server restart
+service rabbitmq-server start
