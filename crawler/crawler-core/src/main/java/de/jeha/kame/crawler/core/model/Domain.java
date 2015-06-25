3438470bad758af1f8451261a248544fcafede9c
jenshadlich <jenshadlich@googlemail.com>
2015-06-25 21:25:13 +0200

    refactor

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Domain.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Domain.java
@@ -1,24 +1,32 @@
 package de.jeha.kame.crawler.core.model;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  * @author jenshadlich@googlemail.com
  */
 public class Domain {
 
     private final Hash hash;
-    private final String domain;
+    private final String name;
 
-    public Domain(String domain) {
-        this.domain = domain;
-        this.hash = new Hash(domain);
+    public Domain(String name) {
+        this.name = name;
+        this.hash = new Hash(name);
     }
 
     public Hash getHash() {
         return hash;
     }
 
-    public String getDomain() {
-        return domain;
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 
 }
