b97455cda64ce2f7df5407cb579073837f48217e
jenshadlich <jenshadlich@googlemail.com>
2015-06-25 18:04:48 +0200

    only add RabbitMQ signing key if it does not exist; show RabbitMQ status

--- crawler/rabbitmq/provision.sh
+++ crawler/rabbitmq/provision.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-apt-key add rabbitmq-signing-key-public.asc
+# install RabbitMQ signing key if it was not already added
+if ! `apt-key list | grep -q "RabbitMQ Release Signing Key <info@rabbitmq.com>"`; then
+    wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+    apt-key add rabbitmq-signing-key-public.asc
+fi
 
 apt-get update > /dev/null
 apt-get -q -y install rabbitmq-server
@@ -11,3 +14,6 @@ apt-get -q -y install rabbitmq-server
 service rabbitmq-server stop
 rabbitmq-plugins enable rabbitmq_management
 service rabbitmq-server start
+
+# show RabbitMQ status
+service rabbitmq-server status
