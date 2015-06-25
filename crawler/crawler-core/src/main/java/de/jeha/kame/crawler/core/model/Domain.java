4c0031fcfd9bd23845667a622cb0692aee8a897d
jenshadlich <jenshadlich@googlemail.com>
2015-06-25 20:34:54 +0200

    implement

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Domain.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Domain.java
@@ -4,4 +4,21 @@ package de.jeha.kame.crawler.core.model;
  * @author jenshadlich@googlemail.com
  */
 public class Domain {
+
+    private final Hash hash;
+    private final String domain;
+
+    public Domain(String domain) {
+        this.domain = domain;
+        this.hash = new Hash(domain);
+    }
+
+    public Hash getHash() {
+        return hash;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
 }
--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Page.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Page.java
@@ -6,9 +6,25 @@ package de.jeha.kame.crawler.core.model;
 public class Page {
 
     private final Domain domain;
+    private final Hash hash;
+    private final String url;
 
-    public Page(Domain domain) {
+    public Page(Domain domain, String url) {
         this.domain = domain;
+        this.hash = new Hash(url);
+        this.url = url;
+    }
+
+    public Domain getDomain() {
+        return domain;
+    }
+
+    public Hash getHash() {
+        return hash;
+    }
+
+    public String getUrl() {
+        return url;
     }
 
 }
