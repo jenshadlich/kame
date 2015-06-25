3dbe697c6147673be1ca0dee06bf53d7851c74f0
jenshadlich <jenshadlich@googlemail.com>
2015-06-25 21:25:48 +0200

    add test

---
+++ crawler/crawler-core/src/test/java/de/jeha/kame/crawler/core/model/PageBuilderTest.java
@@ -0,0 +1,26 @@
+package de.jeha.kame.crawler.core.model;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class PageBuilderTest {
+
+    @Test
+    public void test() throws IOException {
+        String url = "http://www.foo.bar/baz";
+        String content = IOUtils.toString(this.getClass().getResourceAsStream("/simple.html"));
+
+        Page page = Page.Builder.New().withUrl(url).withContent(content).build();
+
+        assertEquals("foo.bar", page.getDomain().getName());
+        assertEquals(1, page.getLinks().size());
+    }
+
+}
