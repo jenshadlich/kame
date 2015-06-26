5fe93ba1bff05a022bce477c070b91562372fe41
jenshadlich <jenshadlich@googlemail.com>
2015-06-26 09:41:39 +0200

    provision mongodb 3.0.4

--- crawler/mongodb/provision.sh
+++ crawler/mongodb/provision.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-echo TODO
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+sudo apt-get update
+
+# install mongodb 3.0.4
+sudo apt-get install -y mongodb-org=3.0.4 mongodb-org-server=3.0.4 mongodb-org-shell=3.0.4 mongodb-org-mongos=3.0.4 mongodb-org-tools=3.0.4
+
+# keep version mongodb 3.0.4
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+cat /var/log/mongodb/mongod.log
