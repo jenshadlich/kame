19dd80e967b6b9b8f38334015f0d79b385f2929a
jenshadlich <jenshadlich@googlemail.com>
2015-06-27 21:53:13 +0200

    add link to model

---
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Link.java
@@ -0,0 +1,18 @@
+package de.jeha.kame.crawler.core.model;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class Link {
+
+    private final String value;
+
+    public Link(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
