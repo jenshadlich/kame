44ed71fc2b55a69401cec31beca1e4de4373eddc
jenshadlich <jenshadlich@googlemail.com>
2015-06-29 08:52:23 +0200

    add special case for null or empty string

--- crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Hash.java
+++ crawler/crawler-core/src/main/java/de/jeha/kame/crawler/core/model/Hash.java
@@ -4,6 +4,7 @@ import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -14,20 +15,27 @@ import java.security.NoSuchAlgorithmException;
 public class Hash {
 
     private static final MessageDigest MESSAGE_DIGEST;
+    private static final String NULL_OR_EMPTY_STRING_HASH;
 
     static {
         try {
             MESSAGE_DIGEST = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
+            NULL_OR_EMPTY_STRING_HASH = Hex.encodeHexString(MESSAGE_DIGEST.digest("".getBytes("UTF-8")));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
+
     }
 
     private final String value;
 
     public Hash(String input) {
-        byte[] digest = MESSAGE_DIGEST.digest((input == null ? "" : input).getBytes(Charset.forName("UTF-8")));
-        this.value = Hex.encodeHexString(digest);
+        if (input == null || input.length() == 0) {
+            this.value = NULL_OR_EMPTY_STRING_HASH;
+        } else {
+            byte[] digest = MESSAGE_DIGEST.digest(input.getBytes(Charset.forName("UTF-8")));
+            this.value = Hex.encodeHexString(digest);
+        }
     }
 
     public String getValue() {
