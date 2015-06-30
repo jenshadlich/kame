68a5274ed5d1269c7a5e1fe67ee42672afa84df0
jenshadlich <jenshadlich@googlemail.com>
2015-06-30 17:03:02 +0200

    add

---
+++ crawler/crawler-scheduler/src/main/java/de/jeha/kame/crawler/scheduler/services/SchedulerException.java
@@ -0,0 +1,12 @@
+package de.jeha.kame.crawler.scheduler.services;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class SchedulerException extends Exception {
+
+    public SchedulerException(Exception e) {
+        super(e);
+    }
+
+}
