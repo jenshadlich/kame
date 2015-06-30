4f6863f7712bc1ba00723a45d86df4c4e9f447e9
jenshadlich <jenshadlich@googlemail.com>
2015-06-30 17:24:04 +0200

    add skeleton for a quartz-based crawl job

---
+++ crawler/crawler-scheduler/src/main/java/de/jeha/kame/crawler/scheduler/services/QuartzCrawlJob.java
@@ -0,0 +1,25 @@
+package de.jeha.kame.crawler.scheduler.services;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+class QuartzCrawlJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QuartzCrawlJob.class);
+
+    public static final String NAME = "NAME";
+    public static final String SEED_URL = "SEED_URL";
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        LOG.info("{}", context.get(NAME));
+        LOG.info("{}", context.get(SEED_URL));
+    }
+
+}
