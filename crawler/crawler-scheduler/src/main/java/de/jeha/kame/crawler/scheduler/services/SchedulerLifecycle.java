dd4a854ca6cde301a3b2a1ab47f350b8c84a1278
jenshadlich <jenshadlich@googlemail.com>
2015-06-30 07:26:32 +0200

    provide interface for scheduler lifecycle management

---
+++ crawler/crawler-scheduler/src/main/java/de/jeha/kame/crawler/scheduler/services/SchedulerLifecycle.java
@@ -0,0 +1,12 @@
+package de.jeha.kame.crawler.scheduler.services;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public interface SchedulerLifecycle {
+
+    public void start();
+
+    public void suspend();
+
+}
