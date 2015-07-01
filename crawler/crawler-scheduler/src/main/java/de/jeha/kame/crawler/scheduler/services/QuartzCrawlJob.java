6e8c38d7030486dde0e9b044ef2401df1c8fa363
jenshadlich <jenshadlich@googlemail.com>
2015-07-01 22:28:43 +0200

    call crawler service (wip hack)

--- crawler/crawler-scheduler/src/main/java/de/jeha/kame/crawler/scheduler/services/QuartzCrawlJob.java
+++ crawler/crawler-scheduler/src/main/java/de/jeha/kame/crawler/scheduler/services/QuartzCrawlJob.java
@@ -1,11 +1,22 @@
 package de.jeha.kame.crawler.scheduler.services;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.jeha.kame.crawler.core.http.ContentType;
+import de.jeha.kame.crawler.core.http.Headers;
 import de.jeha.kame.crawler.scheduler.model.CrawlJob;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * @author jenshadlich@googlemail.com
@@ -20,6 +31,39 @@ public class QuartzCrawlJob implements Job {
     public void execute(JobExecutionContext context) throws JobExecutionException {
         CrawlJob crawlJob = (CrawlJob) context.getJobDetail().getJobDataMap().get(CRAWL_JOB);
         LOG.info("{}", crawlJob);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        CrawlRequest request = new CrawlRequest(crawlJob.getSeedUrl());
+
+        HttpPost httpPost = new HttpPost("http://localhost:8080/crawl");
+        httpPost.setHeader(Headers.CONTENT_TYPE, ContentType.APPLICATION_JSON.getValue());
+
+        try {
+            httpPost.setEntity(new StringEntity(objectMapper.writeValueAsString(request), "UTF-8"));
+            HttpResponse response = HttpClients.createDefault().execute(httpPost);
+
+            String responseJson = EntityUtils.toString(response.getEntity());
+            LOG.info("{}", responseJson);
+
+        } catch (IOException e) {
+            throw new JobExecutionException(e);
+        }
+
+    }
+
+    public class CrawlRequest {
+
+        @JsonProperty
+        private String url;
+
+        public CrawlRequest(String url) {
+            this.url = url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
     }
 
 }
