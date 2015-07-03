197769f26d932bcec48b0f288e73ad18cd5bbebf
jenshadlich <jenshadlich@googlemail.com>
2015-07-03 18:19:09 +0200

    use crawler service client from outer module

--- crawler/crawler-scheduler/src/main/java/de/jeha/kame/crawler/scheduler/services/QuartzCrawlJob.java
+++ crawler/crawler-scheduler/src/main/java/de/jeha/kame/crawler/scheduler/services/QuartzCrawlJob.java
@@ -6,6 +6,7 @@ import de.jeha.kame.crawler.core.http.Headers;
 import de.jeha.kame.crawler.scheduler.model.CrawlJob;
 import de.jeha.kame.crawler.service.api.CrawlRequest;
 import de.jeha.kame.crawler.service.api.CrawlResponse;
+import de.jeha.kame.crawler.service.client.CrawlerServiceClient;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -40,30 +41,6 @@ public class QuartzCrawlJob implements Job {
             LOG.info("{}", response.getId());
         } catch (IOException e) {
             throw new JobExecutionException(e);
-        }
-
-    }
-
-    // -----------------------------------------------------------------------------------------------------------------
-
-    static class CrawlerServiceClient {
-
-        private final String endpointUrl;
-        private final ObjectMapper objectMapper = new ObjectMapper();
-
-        public CrawlerServiceClient(String endpointUrl) {
-            this.endpointUrl = endpointUrl;
-        }
-
-        public CrawlResponse crawl(CrawlRequest request) throws IOException {
-            HttpPost httpPost = new HttpPost(endpointUrl + "/crawl");
-            httpPost.setHeader(Headers.CONTENT_TYPE, ContentType.APPLICATION_JSON.getValue());
-
-            httpPost.setEntity(new StringEntity(objectMapper.writeValueAsString(request), "UTF-8"));
-            HttpResponse httpResponse = HttpClients.createDefault().execute(httpPost);
-
-            String responseJson = EntityUtils.toString(httpResponse.getEntity());
-            return objectMapper.readValue(responseJson, CrawlResponse.class);
         }
 
     }
